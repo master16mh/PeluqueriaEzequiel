8ef2cd93dc71512c79f707eed7809049c3ed17d0
master16mh <master16mh@gmail.com>
2026-05-25 17:50:05 -0300

    proyecto peluqueria

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import peluqueriacalidad from './assets/peluqueriacalidad.jpeg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+        </div>
+        <div className='principal'>
+          <img className="logoPeluqueria" src={peluqueriacalidad} alt="Peluqueria"></img>
+          <h1>Evolución Peluquería</h1>
+          <h2>Unisex</h2>
+          <h2>♂️♀️</h2>
+        </div>
+      </section>
+      <section>
+        <div>
+          <footer>Desde el 2000</footer>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
