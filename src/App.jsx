6dc14d4aa9a230b255b304b1cc50ae452c5dc9d9
master16mh <master16mh@gmail.com>
2026-05-29 17:31:54 -0300

    trayendo boton susin

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import peluqueriacalidad from './assets/peluqueriacalidad.jpeg'
 import './App.css'
 
